1779e703dc8118f4010bc2cd3e43eb8bc7cd4d64
Austin <pizzahi5@gmail.com>
2022-10-31 14:11:15 -0700

    Adding_safeMath

--- src/FortStrings.sol
+++ src/FortStrings.sol
@@ -8,6 +8,7 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
  * @dev String operations.
  */
 library Strings {
+    using SafeMath for uint256;
 
     bytes16 private constant _SYMBOLS = "0123456789abcdef";
     uint8 private constant _ADDRESS_LENGTH = 20;
@@ -40,7 +41,7 @@ library Strings {
 
     // Need to add SafeMath to these operations
     function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
-        bytes memory buffer = new bytes(2 * length + 2);
+        bytes memory buffer = new bytes(length.mul(2) + 2);
         buffer[0] = "0";
         buffer[1] = "x";
         for (uint256 i = 2 * length + 1; i > 1; --i) {
@@ -50,4 +51,13 @@ library Strings {
         require(value == 0, "Strings: hex length insufficient");
         return string(buffer);
     }
+    /*
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+    */
 }
