b7abfbd4fa02b01352565a2cdb23c9109d9b873c
Austin <pizzahi5@gmail.com>
2022-11-01 10:45:22 -0700

    Wrote_test_and_added_SafeMathUpgradeable

--- src/FortStrings.sol
+++ src/FortStrings.sol
@@ -2,13 +2,13 @@
 
 pragma solidity ^0.7.0;
 
-import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { SafeMathUpgradeable } from "@openzeppelin-upgradeable/contracts/math/SafeMathUpgradeable.sol";
 
 /**
  * @dev String operations.
  */
 library Strings {
-    using SafeMath for uint256;
+    using SafeMathUpgradeable for uint256;
 
     bytes16 private constant _SYMBOLS = "0123456789abcdef";
     uint8 private constant _ADDRESS_LENGTH = 20;
@@ -39,25 +39,16 @@ library Strings {
         return string(buffer);
     }
 
-    // Need to add SafeMath to these operations
+    // * @dev Added SafeMathUpgradeable to these operations, ref v0.8.0 Strings
     function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
-        bytes memory buffer = new bytes(length.mul(2) + 2);
+        bytes memory buffer = new bytes(length.mul(2).add(2));
         buffer[0] = "0";
         buffer[1] = "x";
-        for (uint256 i = 2 * length + 1; i > 1; --i) {
+        for (uint256 i = length.mul(2).add(1); i > 1; i.sub(1)) {
             buffer[i] = _SYMBOLS[value & 0xf];
             value >>= 4;
         }
         require(value == 0, "Strings: hex length insufficient");
         return string(buffer);
     }
-    /*
-        bytes memory buffer = new bytes(2 * length + 2);
-        buffer[0] = "0";
-        buffer[1] = "x";
-        for (uint256 i = 2 * length + 1; i > 1; --i) {
-            buffer[i] = _SYMBOLS[value & 0xf];
-            value >>= 4;
-        }
-    */
 }
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,19 @@
 pragma solidity 0.7.6;
 
 import "forge-std/Test.sol";
+import "src/FortStrings.sol";
+import { SafeMathUpgradeable } from "@openzeppelin-upgradeable/contracts/math/SafeMathUpgradeable.sol";
 
 contract ContractTest is Test {
+    using SafeMathUpgradeable for uint256;
+
     function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    function testStrings() public {
+        uint256 length = 2;
+        emit log_uint(length);
+        length = length.mul(2).add(2);
+        emit log_uint(length);
+        assertEq(length, 6);
     }
 }
